cb128afaf9fec54873ef648794fe71c640aae28d
GiuliaSossai <giulia.sossai@yahoo.it>
2022-02-28 10:01:11 +0100

    inizio messaggi errore ita

--- app/Http/Requests/ValidationRestaurant.php
+++ app/Http/Requests/ValidationRestaurant.php
@@ -24,16 +24,26 @@ class ValidationRestaurant extends FormRequest
     public function rules()
     {
         return [
-            "name" => "required|min:2|max:100",
-            "address" => "required|max:255",
-            "email" => "required|email|max:255",
-            "p_iva" => "required|max:20",
-            "phone" => "required|max:25",
-            "delivery_price" => "numeric|between:0, 99",
-            "order_min" => "numeric|between: 0, 255",
-            "delivery_time" => "numeric|between: 0, 200",
-            "photo" => "mimes:jpeg,png,jpg,gif|max:2048",
-            "photo_bg" => "mimes:jpeg,png,jpg,gif|max:32000",
+                "name" => "required|min:2|max:100",
+                "address" => "required|max:255",
+                "email" => "required|email|max:255",
+                "p_iva" => "required|max:20",
+                "phone" => "required|max:25",
+                "delivery_price" => "numeric|between:0, 99",
+                "order_min" => "numeric|between: 0, 255",
+                "delivery_time" => "numeric|between: 0, 200",
+                "photo" => "mimes:jpeg,png,jpg,gif|max:2048",
+                "photo_bg" => "mimes:jpeg,png,jpg,gif|max:32000"
+            
+            
+        ];
+    }
+
+    public function messages(){
+        return [
+            'name.required' => "Il nome è obbligatorio",
+            'name.min' => "Il nome deve avere almeno :min caratteri",
+            
         ];
     }
 }
