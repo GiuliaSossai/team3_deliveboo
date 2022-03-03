efaad0a12bb61c65d6f4c3212625c61a6e45229e
GiuliaSossai <giulia.sossai@yahoo.it>
2022-03-03 09:54:25 +0100

    funzione per filtrare per categorie e per ri-ottenere tutti i ristoranti

--- resources/js/components/pages/restaurants/Restaurants.vue
+++ resources/js/components/pages/restaurants/Restaurants.vue
@@ -9,11 +9,25 @@
                 <div
                     class="col"
                     v-for="category in categories"
-                    :key="category.id"
+                    :key="`cat${category.id}`"
                 >
-                    <CategoryCard :category="category" />
+                    <CategoryCard 
+                        :category="category" 
+                        @getCategory = "getRestaurantsByCategory"
+                    />
+                </div>
+
+                <div class="col">
+                    <div class="card-all"
+                        @click="getApi"
+                    >
+                        <div class="card-cat__text">
+                            <p>tutti i ristoranti</p>
+                        </div>   
+                    </div>
                 </div>
             </div>
+
             <h2>I Ristoranti</h2>
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 mb-5">
                 <!-- <div class="col d-flex">
@@ -22,7 +36,7 @@
                 <div
                     class="col d-flex justify-content-center"
                     v-for="restaurant in restaurants"
-                    :key="restaurant.id"
+                    :key="`rest${restaurant.id}`"
                 >
                     <RestaurantCard :restaurant="restaurant" />
                 </div>
@@ -58,17 +72,31 @@ export default {
 
     methods: {
         getApi() {
+            this.restaurants = [];
             axios
                 .get(this.apiUrl)
                 .then((res) => {
                     this.restaurants = res.data.restaurants;
                     this.categories = res.data.categories;
-                    console.log(this.categories);
+                    //console.log(this.categories);
                 })
                 .catch((error) => {
                     console.log(error);
                 });
         },
+
+        getRestaurantsByCategory($slug){
+            this.restaurants = [];
+            //console.log($slug);
+            axios.get(this.apiUrl + '/restaurantcategory/' + $slug)
+            .then( res => {
+                this.restaurants = res.data.category.restaurants;
+                //console.log(res.data.category.restaurants);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
     },
 };
 </script>
@@ -76,5 +104,27 @@ export default {
 <style lang="scss" scoped>
 main {
     margin-top: 90px;
+    .card-all {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 220px;
+        height: 80px;
+        padding: 15px 5px;
+        background-color: rgb(246, 246, 246);
+        color: rgb(96, 218, 96);
+        text-decoration: none;
+        border-radius: 15px;
+        margin-bottom: 15px;
+        text-transform: uppercase;
+        cursor: pointer;
+        .card-cat__text {
+            p {
+                margin: 0;
+                font-size: 20px;
+                font-weight: 500;
+            }
+        }
+    }
 }
 </style>
