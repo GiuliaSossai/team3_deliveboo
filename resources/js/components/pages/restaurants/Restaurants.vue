19956b66fa5c0b745b0e5d254adab1e524f89aad
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2022-03-03 18:06:20 +0100

    Creazione pagina loading e 404

--- resources/js/components/pages/restaurants/Restaurants.vue
+++ resources/js/components/pages/restaurants/Restaurants.vue
@@ -1,61 +1,77 @@
 <template>
-    <main>
-        <FirstSection />
-        <div class="container">
-            <h2 class="mb-4">Esplora per Categorie</h2>
-            <div
-                class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 my-5"
-            >
-                <div
-                    class="col"
-                    v-for="category in categories"
-                    :key="`cat${category.id}`"
-                >
-                    <CategoryCard 
-                        :category="category" 
-                        @getCategory = "getRestaurantsByCategory"
-                    />
-                </div>
-
-                <div class="col">
-                    <div class="card-all"
-                        @click="getApi"
+    <div>
+        <div v-if="loading">
+            <Loading />
+        </div>
+        <div v-else>
+            <Header />
+            <main>
+                <FirstSection />
+                <div class="container">
+                    <h2 class="mb-4">Esplora per Categorie</h2>
+                    <div
+                        class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 my-5"
                     >
-                        <div class="card-cat__text">
-                            <p>tutti i ristoranti</p>
-                        </div>   
-                    </div>
-                </div>
-            </div>
+                        <div
+                            class="col"
+                            v-for="category in categories"
+                            :key="`cat${category.id}`"
+                        >
+                            <CategoryCard
+                                :category="category"
+                                @getCategory="getRestaurantsByCategory"
+                            />
+                        </div>
 
-            <h2>I Ristoranti</h2>
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 mb-5">
-                <!-- <div class="col d-flex">
+                        <div class="col">
+                            <div class="card-all" @click="getApi">
+                                <div class="card-cat__text">
+                                    <p>tutti i ristoranti</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <h2>I Ristoranti</h2>
+                    <div
+                        class="row row-cols-1 row-cols-md-2 row-cols-lg-3 mb-5"
+                    >
+                        <!-- <div class="col d-flex">
                     <RestaurantCard />
                 </div> -->
-                <div
-                    class="col d-flex justify-content-center"
-                    v-for="restaurant in restaurants"
-                    :key="`rest${restaurant.id}`"
-                >
-                    <RestaurantCard :restaurant="restaurant" />
+                        <div
+                            class="col d-flex justify-content-center"
+                            v-for="restaurant in restaurants"
+                            :key="`rest${restaurant.id}`"
+                        >
+                            <RestaurantCard :restaurant="restaurant" />
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </main>
+
+            <Footer />
         </div>
-    </main>
+    </div>
 </template>
 
 <script>
+import Header from "../../partials/Header.vue";
 import FirstSection from "./FirstSection.vue";
 import CategoryCard from "./CategoryCard.vue";
 import RestaurantCard from "./RestaurantCard.vue";
+import Footer from "../../partials/Footer.vue";
+import Loading from "../Loading.vue";
 
 export default {
     name: "Restaurants",
     components: {
+        Header,
         FirstSection,
         CategoryCard,
         RestaurantCard,
+        Footer,
+        Loading,
     },
 
     data() {
@@ -63,6 +79,7 @@ export default {
             categories: [],
             apiUrl: "/api/restaurants",
             restaurants: [],
+            loading: true,
         };
     },
 
@@ -79,24 +96,26 @@ export default {
                     this.restaurants = res.data.restaurants;
                     this.categories = res.data.categories;
                     //console.log(this.categories);
+                    setTimeout(() => (this.loading = false), 3000);
                 })
                 .catch((error) => {
                     console.log(error);
                 });
         },
 
-        getRestaurantsByCategory($slug){
+        getRestaurantsByCategory($slug) {
             this.restaurants = [];
             //console.log($slug);
-            axios.get(this.apiUrl + '/restaurantcategory/' + $slug)
-            .then( res => {
-                this.restaurants = res.data.category.restaurants;
-                //console.log(res.data.category.restaurants);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        }
+            axios
+                .get(this.apiUrl + "/restaurantcategory/" + $slug)
+                .then((res) => {
+                    this.restaurants = res.data.category.restaurants;
+                    //console.log(res.data.category.restaurants);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
     },
 };
 </script>
