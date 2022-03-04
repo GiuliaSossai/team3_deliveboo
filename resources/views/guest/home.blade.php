46168e19e5dd1c8fb5f36f06f7c73aa95f5b48b2
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2022-03-04 09:45:06 +0100

    Fine divisione ambienti

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="{{asset ("css/guest/style.css")}}">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Deliveboo</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/guest/style.css') }}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Deliveboo</title>
 </head>
+
 <body>
-  <div id="app">
-    
-  </div>
-  <script src="{{asset ("js/app.js")}}"></script>
+    <div id="app">
+
+    </div>
+    <script src="{{ asset('js/guest/guest.js') }}"></script>
 </body>
+
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/admin.js', 'public/js/admin')
+    .js('resources/js/guest.js', 'public/js/guest')
     .sass('resources/sass/admin/app.scss', 'public/css/admin')
     .sass('resources/sass/guest/style.scss', 'public/css/guest')
     .options({
