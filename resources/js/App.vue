3bc1fd812c29a6b34aba49faa168f3da5bd995cc
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2022-03-05 04:56:49 +0100

    Creata una finestra cookie

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
         <router-view></router-view>
+        <Cookie v-if="clickCookie == false" @getCookie="getCookie" />
     </div>
 </template>
 
 <script>
+import Cookie from "./components/partials/Cookie.vue";
 export default {
     name: "App",
+    components: {
+        Cookie,
+    },
+    data() {
+        return {
+            clickCookie: false,
+        };
+    },
+    computed: {},
+    mounted() {},
+    methods: {
+        getCookie(value) {
+            this.clickCookie = value;
+        },
+    },
 };
 </script>
 
---
+++ resources/js/components/partials/Cookie.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <div class="ls-cookie">
+            <h3>Utilizziamo i cookie</h3>
+            <p>
+                Fai clic su "Accetta" per consentire a deliveBoo di utilizzare i
+                cookie per personalizzare questo sito, pubblicare annunci e
+                misurarne l'efficacia su altre app e siti web, compresi i social
+                media. Se non desideri che i cookie vengano impiegati a tale
+                scopo, personalizza le tue preferenze nelle impostazioni dei
+                cookie o fai clic su "Rifiuta". Ulteriori informazioni nella
+                sezione <a href="#">Informativa sui cookie</a>.
+            </p>
+            <div class="ls-btn-container">
+                <div class="ls-imp">Impostazioni dei cookie</div>
+                <div class="ls-rif" @click="getCookie">Rifiuta</div>
+                <div class="ls-acc" @click="getCookie">Accetta</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Cookie",
+    data() {
+        return {};
+    },
+    methods: {
+        getCookie() {
+            this.$emit("getCookie", true);
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.ls-cookie {
+    position: fixed;
+    bottom: 50px;
+    right: 50px;
+    z-index: 10;
+    max-width: 500px;
+    min-height: 180px;
+    padding: 25px;
+    font-size: 12px;
+    box-shadow: 0px 0px 20px rgb(0 0 0 / 8%);
+    background-color: #ffffff;
+    .ls-btn-container {
+        display: flex;
+        justify-content: flex-end;
+        .ls-imp {
+            padding: 8px 22px;
+            cursor: pointer;
+            &:hover {
+                background: #cccccc;
+            }
+        }
+        .ls-rif {
+            padding: 8px 18px;
+            background: #eeeeee;
+            margin-left: 15px;
+            font-size: 14px;
+            font-weight: 400;
+            cursor: pointer;
+            &:hover {
+                background: #e2e2e2;
+            }
+        }
+        .ls-acc {
+            padding: 8px 30px;
+            color: white;
+            background: #000000;
+            margin-left: 15px;
+            font-size: 14px;
+            font-weight: 600;
+            cursor: pointer;
+            &:hover {
+                background: #333333;
+            }
+        }
+    }
+}
+</style>
