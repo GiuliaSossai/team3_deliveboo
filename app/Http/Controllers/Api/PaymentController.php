9f7f9af28b5e83f2df90365593f5ba536e6524f6
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2022-03-08 15:29:50 +0100

    commit prima fase

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -2,25 +2,48 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Braintree\Gateway;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ValidationOrder;
 
 class PaymentController extends Controller
 {
-    public function generate(Request $request, Gateway $gateway){
-        // $gateway = new Gateway([
-        //     'environment' => 'sandbox',
-        //     'merchantId' => '64nmzg3dhd34vvh7',
-        //     'publicKey' => 'gyy6c2h63b9hc5x3',
-        //     'privateKey' => '5e0a37a0db2beef5cb29a7a131cd84c5'
-        // ]);
-        dd($gateway->clientToken()->generate());
-        return 'generate';
+    public function generate(Request $request, Gateway $gateway)
+    {
+        $token = $gateway->clientToken()->generate();
+
+        $data = [
+            'success' => true,
+            'token' => $token,
+        ];
+
+        return response()->json($data, 200);
     }
 
-    public function makePayment(Request $request)
+    public function makePayment(ValidationOrder $request,  Gateway $gateway)
     {
-        return 'makePayment';
+
+        $result = $gateway->transaction()->sale([
+            'amount' => $request->amount,
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+                'submitForSettlement' => true
+            ]
+        ]);
+
+        if ($result->success) {
+            $data = [
+                'success' => true,
+                'message' => "Transazione eseguita con Successo!"
+            ];
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'success' => false,
+                'message' => "Transazione Fallita!!"
+            ];
+            return response()->json($data, 401);
+        }
     }
 }
