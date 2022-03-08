b5de6d7a28319d0b97fc28a06e4f1cbb774ec085
TonyBrb <anthonybarbagallo95@gmail.com>
2022-03-08 12:16:31 +0100

    inizio braintree

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Braintree\Gateway;
 
 class PaymentController extends Controller
 {
-    public function generate(Request $request){
+    public function generate(Request $request, Gateway $gateway){
+        // $gateway = new Gateway([
+        //     'environment' => 'sandbox',
+        //     'merchantId' => '64nmzg3dhd34vvh7',
+        //     'publicKey' => 'gyy6c2h63b9hc5x3',
+        //     'privateKey' => '5e0a37a0db2beef5cb29a7a131cd84c5'
+        // ]);
+        dd($gateway->clientToken()->generate());
         return 'generate';
     }
 
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Braintree\Gateway;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(Gateway::class, function($app){
+            return new Gateway([
+                  'enviroment' => 'sandbox',
+                  'merchantId' => '64nmzg3dhd34vvh7',
+                  'publicKey' => 'gyy6c2h63b9hc5x3',
+                  'privateKey' => '5e0a37a0db2beef5cb29a7a131cd84c5'
+                ]
+                );
+        });
     }
 }
+
