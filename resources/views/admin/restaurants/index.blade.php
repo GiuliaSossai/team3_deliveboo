090c7afd54436c6c4dd19cf6dd8226b0c10baff4
G-Salis <giuseppesalislavoro@gmail.com>
2022-03-10 11:15:03 +0100

    Stile Grafica back-end

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,8 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>I miei Ristoranti</h1>
+
+    <div class="container mt-5">
+
+
+
+        <h2>I Ristoranti</h2>
+
+        <a class="btn btn-primary mb-3" href="{{ route('admin.ristoranti.create') }}" role="button">Aggiungi un nuovo ristorante</a>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 mb-5">
+        @foreach ($restaurants as $restaurant)
+            <div class="card-rest-box p-3">
+                <div class="translate">
+
+                    <a href=""></a>
+
+                    <a href="{{ route('admin.ristoranti.show', $restaurant->slug) }}">
+
+                        <div class="card-rest-box-image" >
+                            @if ($restaurant->photo)
+
+                                <img src="{{ asset($restaurant->photo) }}" alt="#">
+                            @else
+                                <p>Nessuna foto presente</p>
+                            @endif
+                            
+                        </div>
+                    
+                    </a>
+                
+
+                    <div class="card-rest-box-text">
+                        <h5>{{ $restaurant->name }}</h5>
+                    </div>
+
+                </div>
+                
+                <div class="d-flex mt-3 ">
+
+                    <a class="btn btn-success mr-3" href="{{ route('admin.ristoranti.edit', $restaurant->slug) }}" role="button">Modifica</a>
+                    <form action="{{ route('admin.ristoranti.destroy', $restaurant) }}" method="post"
+                                    onsubmit="return confirm('Sei sicuro di voler eliminare il ristorante {{ $restaurant->name }}')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Elimina</button>
+                    </form>
+                </div>
+            </div>
+            
+
+
+
+
+        @endforeach
+        
+    </div>
+    </div>
+
+        {{-- <h1>I miei Ristoranti</h1>
 
         <td> <a class="btn btn-primary mb-3" href="{{ route('admin.ristoranti.create') }}" role="button">Aggiungi un nuovo
                 ristorante</a>
@@ -44,7 +100,8 @@
                 @endforeach
 
             </tbody>
-        </table>
+        </table> --}}
 
     </div>
+
 @endsection
