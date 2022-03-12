1a963dd76fe0ff508df10743e7cddd82d00fe628
GiuliaSossai <giulia.sossai@yahoo.it>
2022-03-12 12:28:07 +0100

    sistemazione dashboard e header - fontawesome non va

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,42 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="gs-container">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card p-4">
+                        {{-- <div class="card-header">{{ __('Dashboard') }}</div> --}}
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <div class="row">
-                            <div class="col-6">
-                                Nome Utente
-                                FOto
-                            </div>
-                            <div class="col-6">
-                                I tuoi Guadagni
-                            </div>
+                        <div class="card-body text-center">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            {{-- <div class="row">
+                                <div class="col-6">
+                                    Nome Utente
+                                    FOto
+                                </div>
+                                <div class="col-6">
+                                    I tuoi Guadagni
+                                </div>
+                            </div> --}}
+                            {{-- <div class="row">
+                                <div class="col-6">
+                                    Vai ai ristoranti
+                                </div>
+                                <div class="col-6">
+                                    Statistiche
+                                </div>
+                            </div> --}}
+                            <h2>
+                                Benvenuto <span class="font-weight-bolder">{{ Auth::user()->name }}</span> {{ __('nella tua area personale!') }}
+                            </h2>
                         </div>
-                        <div class="row">
-                            <div class="col-6">
-                                Vai ai ristoranti
-                            </div>
-                            <div class="col-6">
-                                Statistiche
-                            </div>
+                        {{-- <a href="{{ route('admin.ristoranti.index') }}" class="btn w-25 m-2 gs-btn">I tuoi ristoranti</a> --}}
+                        <div class="p-4 gs-card-text">
+                            <span><i class="fa-solid fa-arrow-right"></i></span>
+                            <span><a href="{{ route('admin.ristoranti.index') }}" class="gs-link">I tuoi ristoranti</a></span>
                         </div>
-                        Ciao
-                        {{ Auth::user()->name }}
-                        {{ __('You are logged in!') }}
+                        
                     </div>
-                    <a href="{{ route('admin.ristoranti.index') }}" class="btn w-25 btn-primary m-2">Vai ai Ristoranti</a>
                 </div>
             </div>
         </div>
     </div>
+    
 @endsection
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -11,7 +11,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon">Ciao</span>
+                <span class="navbar-toggler-icon text-white"><i class="fa-solid fa-bars"></i></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
